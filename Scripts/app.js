6090b9062f7a7fce103a18c2d64b12fa1f67da35
kushanJ <kushan.joshi@gmail.com>
2016-08-17 14:15:37 -0400

    docx

--- Scripts/app.js
+++ Scripts/app.js
@@ -13,7 +13,19 @@ var core;
         createjs.Ticker.framerate = 60;
         createjs.Ticker.on("tick", gameLoop);
 
+	var ctx=canvas.getContext("2d");
+    	var img1=document.getElementById("die1");
+	img1.src = "Assets/images/1.png"
+	
 
+    	var img2=document.getElementById("die1");
+	img2.src = "Assets/images/6.png"
+
+	var bitmap1 = new createjs.Bitmap(img1);
+	stage.addChild(bitmap1);
+	var bitmap2 = new createjs.Bitmap(img2);
+	stage.addChild(bitmap2);
+	bitmap2.x = 200;	
 
         main();
     }
@@ -25,11 +37,11 @@ var core;
 	
 	var ctx=canvas.getContext("2d");
     	var img1=document.getElementById("die1");
-	img1.src = "Assets/images/1.png"
+
 	
 
-    	var img2=document.getElementById("die1");
-	img2.src = "Assets/images/6.png"
+    	var img2=document.getElementById("die2");
+
 	
 	
 	
@@ -40,41 +52,17 @@ var core;
 		console.log(typeof die1);
 		var die2 = Math.floor((Math.random() * 6) + 1);
 		console.log(die2);
-	if(die1 == 1 || die2 == 1){
-			img1.src = "Assets/images/1.png";
-			img2.src = "Assets/images/1.png";
-			
-	}
-
-	
-	if(die1 == 1 || die2 == 1){
-					img1.src = "Assets/images/1.png";
-			img2.src = "Assets/images/1.png";
-	}
-
-		if(die1 == 2 || die2 == 2){
-					img1.src = "Assets/images/2.png";
-			img2.src = "Assets/images/2.png";
-	}
-
-	if(die1 == 3 || die2 == 3){
-					img1.src = "Assets/images/3.png";
-			img2.src = "Assets/images/3.png";
-	}
-
-	if(die1 == 4 || die2 == 4){
-					img1.src = "Assets/images/4.png";
-			img2.src = "Assets/images/4.png";
-	}
-
-	if(die1 == 5 || die2 == 5){
-					img1.src = "Assets/images/5.png";
-			img2.src = "Assets/images/5.png";
-	}
-
-	if(die1 == 6 || die2 == 6){
-					img1.src = "Assets/images/6.png";
-			img2.src = "Assets/images/6.png";
+	for(var i = 0; i <=6 ; i++){
+		if(die1 == i){
+			console.log("die1 ==" + die1)
+			img1.src = "Assets/images/"+die1+".png";
+			console.log(img1.src)
+		}
+		if(die2 == i){
+			console.log("die2 = " + die2)
+						img2.src = "Assets/images/"+die2+".png";
+			console.log(img2.src)
+		}
 	}
 
 	var bitmap1 = new createjs.Bitmap(img1);
